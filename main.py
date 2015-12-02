4b6a669428f751d3342aa7027dc9e232810b82fd
pillowpilot <carlos.federico005@gmail.com>
2015-12-02 02:31:20 -0300

    Fixing main.py

--- main.py
+++ main.py
@@ -7,36 +7,11 @@ class Searcher(object):
         pass
     
     def greedy(self, origin, target, max_iterations=500):
-        moves = []
-        iterations = 1
-        while origin != target and iterations <= max_iterations:
-            iterations += 1
-
-            possible_moves = []
-            moveUp = origin
-            moveUp.up()
-            moveDown = origin
-            moveDown.down()
+        pass
             
 
 def main():
-    solved_state = Puzzle()
-    puzzle = Puzzle()
-    searcher = Searcher()
-
-    puzzle.up()
-    puzzle.up()
-    puzzle.up()
-
-    print( solved_state )
-    print( puzzle )
-
-    solved, moves = searcher.greedy(solved_state, puzzle)
-
-    print( solved )
-    print( moves  )
-    
-    
+    pass
 
 if __name__ == '__main__':
     main()
